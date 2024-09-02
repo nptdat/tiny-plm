a55b5561e448846eb64d21648185b0cb0fb3467b
dat.nguyen <nptdat@gmail.com>
2024-09-02 06:02:40 +0000

    Chunking cross-encoder prediction

--- src/run_predict_cross_encoder.py
+++ src/run_predict_cross_encoder.py
@@ -5,7 +5,7 @@ from collections import defaultdict
 from logging import basicConfig, getLogger
 from pathlib import Path
 from time import time
-from typing import Any, Dict, Tuple, Union
+from typing import Any, Dict, List, Tuple, Union
 
 import pandas as pd
 import torch
@@ -35,6 +35,14 @@ def save_pkl_gzip(
         filepath_.parent.mkdir(parents=True, exist_ok=True)
     with gzip.open(filepath_, "wb") as f:
         pickle.dump(similarity_scores, f)
+
+
+def get_all_query_ids(cfg: Dict[str, Any]) -> List[int]:
+    with gzip.open(cfg["hard_negative_init_score_file"], "rb") as f:
+        init_scores = pickle.load(f)
+    sorted_qids: List[int] = sorted(init_scores.keys())
+
+    return sorted_qids
 
 
 def load_data(
@@ -74,93 +82,74 @@ def load_data(
     return init_scores, id2query, id2doc
 
 
-def predict(
-    init_scores: Dict[int, Dict[int, float]],
-    model: PreTrainedModel,
-    id2query: Dict[int, str],
-    id2doc: Dict[int, str],
-    batch_size: int,
-    max_len: int,
-) -> dict[int, dict[int, float]]:
-    # Sorted pairs of (query, doc) by text len
-    logger.info("Accumulate lens of (query, doc) pairs...")
-    len_list = []
-    # cnt = 0
-    for qid, data in tqdm(init_scores.items()):
-        doc_ids = list(data.keys())
+def prepare_data(
+    cfg: Dict[str, Any], target_qids: set[int], max_len: int
+) -> tuple[list[tuple[int, int, int]], list[tuple[str, str]]]:
+    logger.info(f"Preparing data for prediction with {len(target_qids)=}...")
+
+    init_scores, id2query, id2doc = load_data(cfg)
+    sorted_qids = sorted(init_scores.keys())
+    logger.info(
+        f"{len(init_scores)=}, {len(sorted_qids)=}, {len(id2query)=}, {len(id2doc)=}"
+    )
+
+    len_triples: list[tuple[int, int, int]] = []
+    for qid in target_qids:
         query = id2query[qid]
-        max_p_len = max_len - len(query)
+        max_p_len = max(max_len - len(query), 0)
+        doc_ids = list(init_scores[qid].keys())
         for doc_id in doc_ids:
+            # NOTE: if the query_text is longer than max_len, then all of the doc_text will be empty.
             doc = id2doc[doc_id][:max_p_len]
-            len_list.append((qid, doc_id, len(query) + len(doc)))
-        # cnt += 1
-        # if cnt >= 50:
-        #     break
-    sorted_len_list = sorted(len_list, key=lambda x: -x[2])
+            len_triples.append((qid, doc_id, len(query) + len(doc)))
+    sorted_len_triples: list[tuple[int, int, int]] = sorted(
+        len_triples, key=lambda x: -x[2]
+    )
+    logger.info(f"{len(sorted_len_triples)=}")
 
     # accumulate all pairs sorted by text len (desc order)
     logger.info("Building (query, doc) pairs ordered by text len...")
-    all_pairs = []
-    for qid, doc_id, text_len in tqdm(sorted_len_list):
+    text_pairs: list[tuple[str, str]] = []
+    for qid, doc_id, text_len in tqdm(sorted_len_triples):
         query = id2query[qid]
-        max_p_len = max_len - len(query)
+        max_p_len = max(max_len - len(query), 0)
         doc = id2doc[doc_id][:max_p_len]
-        all_pairs.append([query, doc])
-    logger.info(f"Num of pairs: {len(all_pairs):,}")
+        text_pairs.append((query, doc))
+    logger.info(f"Num of pairs: {len(text_pairs):,}")
 
+    logger.info("Finished preparing data for prediction!")
+
+    return sorted_len_triples, text_pairs
+
+
+def predict(
+    text_pairs: list[tuple[str, str]], model: PreTrainedModel, batch_size: int
+) -> List[float]:
     # predict sim scores with cross-encoder
     logger.info("Predicting scores with cross-encoder...")
-    scores = model.predict(
-        all_pairs, batch_size=batch_size, show_progress_bar=True
+    pred_scores: List[float] = model.predict(
+        text_pairs, batch_size=batch_size, show_progress_bar=True
     ).tolist()
-    tuple_data: tuple = tuple(zip(*sorted_len_list))
-    (qids, doc_ids, lens_) = tuple_data
     logger.info(
-        f"Finished predicting with cross-encoder! Num of scores: {len(scores)}..."
+        f"Finished predicting with cross-encoder! Num of scores: {len(pred_scores)}..."
     )
+
+    return pred_scores
+
+
+def build_similarity_scores(
+    sorted_len_triples: list[tuple[int, int, int]], pred_scores: list[float]
+) -> dict[int, dict[int, float]]:
+    tuple_data: tuple = tuple(zip(*sorted_len_triples))
+    (qids, doc_ids, lens_) = tuple_data
 
     # return similarity scores
     logger.info("Building similarity_scores...")
     similarity_scores: dict[int, dict[int, float]] = defaultdict(dict)
-    for qid, doc_id, score in zip(qids, doc_ids, scores):
+    for qid, doc_id, score in zip(qids, doc_ids, pred_scores):
         similarity_scores[qid][doc_id] = score
 
-    logger.info("Finished similarity_scores!")
     return similarity_scores
-
-
-# def predict_bak(
-#     init_scores: Dict[int, Dict[int, float]],
-#     model: PreTrainedModel,
-#     id2query: Dict[int, str],
-#     id2doc: Dict[int, str],
-#     batch_size: int,
-#     max_len: int,
-# ) -> dict[int, dict[int, float]]:
-#     similarity_scores = {}
-
-#     for qid, data in tqdm(init_scores.items()):
-#         doc_ids = list(data.keys())
-
-#         query = id2query[qid]
-#         max_p_len = max_len - len(query)
-#         scores = []
-#         for start in range(0, len(doc_ids), batch_size):
-#             end = start + batch_size
-#             doc_ids_ = doc_ids[start:end]
-#             pairs = []
-#             for doc_id in doc_ids_:
-#                 pairs.append([query, id2doc[doc_id][:max_p_len]])
-#             scores_ = model.predict(
-#                 pairs,
-#                 batch_size=1,
-#                 show_progress_bar=False
-#             ).tolist()
-#             scores.extend(scores_)
-
-#         similarity_scores[qid] = dict(zip(doc_ids_, scores))
-
-#     return similarity_scores
 
 
 def main(
@@ -179,31 +168,52 @@ def main(
     cfg = load_yaml(config_file)
     logger.info(f"{cfg=}")
 
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    init_scores, id2query, id2doc = load_data(cfg)
+    max_token_len = cfg["max_token_len"]
+    char_per_token_ratio = cfg["char_per_token_ratio"]
+    max_len = int(max_token_len * char_per_token_ratio)
 
+    device = "cuda" if torch.cuda.is_available() else "cpu"
     logger.info("Initializing Cross-Encoder model...")
     model = CrossEncoder(
         cfg["model_path"],
         num_labels=1,
-        max_length=cfg["max_token_len"],
+        max_length=max_token_len,
         device=device,
     )
 
-    logger.info("Predict similarity scores for (query/doc) pairs...")
-    similarity_scores = predict(
-        init_scores,
-        model,
-        id2query,
-        id2doc,
-        cfg["predict_batch_size"],
-        cfg["max_token_len"],
-    )
-    logger.info(f"similarity_scores={len(similarity_scores):,}")
+    sorted_qids = get_all_query_ids(cfg)
+    logger.info(f"All sorted_qids: {len(sorted_qids):,}!")
 
-    output_file = cfg["hard_negative_cross_encoder_score_file"]
-    logger.info(f"Writing similarity scores to {output_file}")
-    save_pkl_gzip(output_file, similarity_scores)
+    query_from = cfg["predict_query_from"]
+    query_to = cfg["predict_query_to"]
+    if query_to == 0:
+        query_to = len(sorted_qids)
+    logger.info(
+        f"{max_token_len=}, {char_per_token_ratio=}, {max_len=}, {query_from=}, {query_to=}"
+    )
+
+    start = 0
+    chunk_size = cfg["predict_query_chunk"]
+    for idx, start in tqdm(enumerate(range(query_from, query_to, chunk_size))):
+        end = min(start + chunk_size, query_to)
+        if start >= end:
+            break
+
+        logger.info(
+            f"--- Predicting for the chunk {idx}(th), {start=}, {end=}..."
+        )
+        sorted_len_triples, text_pairs = prepare_data(
+            cfg=cfg, target_qids=set(sorted_qids[start:end]), max_len=max_len
+        )
+        pred_scores = predict(text_pairs, model, cfg["predict_batch_size"])
+        similarity_scores = build_similarity_scores(
+            sorted_len_triples, pred_scores
+        )
+
+        output_file_tpl = cfg["hard_negative_cross_encoder_score_file"]
+        output_file = output_file_tpl.format(idx)
+        logger.info(f"Writing similarity scores to {output_file}")
+        save_pkl_gzip(output_file, similarity_scores)
 
     logger.info(f"Finished cross-encoder prediction in {time() - start_time}")
 
